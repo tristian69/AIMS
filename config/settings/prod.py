94fc3bdf09ea8407a5b13bbec746c94a1fb5f520
tristian69 <bkju2010@gmail.com>
2025-08-11 20:21:52 +0900

    chore: add css modify

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -16,4 +16,4 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
