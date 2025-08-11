6d81b2ef96f5babbe284c4b3d3d04dec429d3f85
tristian69 <bkju2010@gmail.com>
2025-08-11 17:07:06 +0900

    chore: normalize line endings to LF

--- config/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-admin.site.site_header = "AIMS 관리자"
-admin.site.site_title = "AIMS 관리 페이지"
-admin.site.index_title = "AIMS 시스템 관리"
--- config/settings/prod.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-# DEBUG = False 가 배포 환경,  EC2 에서 개발 환경으로 True 설정
-DEBUG = False
-
-ALLOWED_HOSTS = ['3.25.53.53',]
-
