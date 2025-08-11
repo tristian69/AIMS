06d05ee87329ea73898c30c9a218b4bbebfcd1ca
tristian69 <bkju2010@gmail.com>
2025-08-11 22:01:05 +0900

    chore: add css modify

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,8 @@ from .base import *
 # DEBUG = False 가 배포 환경,  EC2 에서 개발 환경으로 True 설정
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.25.53.53',]
+#ALLOWED_HOSTS = ['3.25.53.53',]
+ALLOWED_HOSTS = ['aimsmanu.com', 'www.aimsmanu.com',]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
